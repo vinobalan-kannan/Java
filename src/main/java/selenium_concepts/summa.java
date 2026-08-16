0bdaada58f40e57a5f056ceb58cfeb281e14e831
vinobalan-kannan <vinobalan.kannan@gmail.com>
2026-08-16 20:58:17 +0530

    merge conflicts

--- src/main/java/selenium_concepts/summa.java
+++ src/main/java/selenium_concepts/summa.java
@@ -2,15 +2,9 @@ package selenium_concepts;
 
 public class summa {
 
-<<<<<<< HEAD
 	public static void main(String a[]) {
 		System.out.println("New change");
 
 	}
-=======
- public static void main(String a[])
- {
-	 	
- }
->>>>>>> 98243be255e43a0dee10c4ae4e4957c623e7999b
+
 }
